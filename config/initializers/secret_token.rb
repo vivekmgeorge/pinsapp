896ce10ca2ee30684b80ad77604a316106bcaf74
Vivek George <vivek.m.george@gmail.com>
2013-12-07 22:04:49 -0500

    initial committ

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinsapp::Application.config.secret_key_base = '736d21d0227cf7a29e58d1ede50b5bdf759a6dca2f8fd905746936fbc201d02246be5932bdd692bfe5632210940f8de5ad9a8a4a9b0912c9dbf391b8eb2f06b3'
